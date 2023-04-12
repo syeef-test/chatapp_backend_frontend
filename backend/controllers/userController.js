c2c13900083d802588816bcd7d467515e5172d0f
syeef <syeefislam@yahoo.com>
2023-04-12 20:27:16 +0530

    login successful

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,37 +4,63 @@ const jwt = require('jsonwebtoken');
 
 
 exports.postSignup = async (req, res, next) => {
-    try {
-      //console.log(req.body);
-      const name = req.body.name;
-      const email = req.body.email;
-      const password = req.body.password;
-      const phone = req.body.phonenumber;
+  try {
+    //console.log(req.body);
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const phone = req.body.phonenumber;
 
-      //console.log(req.body.email);
-  
-      const user = await User.findAll({
-        attributes: ["email"],
-        where: { email: req.body.email },
-      });
-  
-      if (typeof user !== "undefined" && user.length > 0) {
-        res.status(401).json({ message: "User already exists, Please Login",success:false});
-      } else {
+    //console.log(req.body.email);
 
-        const saltRounds = 10;
-        const hash = await bcrypt.hash(password, saltRounds);
-        if(hash){
-            const insertData = await User.create({ name, email, password: hash ,phone:phone});
-            res.status(201).json({ message: "Sign Up Succesful",success:true}); 
-        }     
+    const user = await User.findAll({
+      attributes: ["email"],
+      where: { email: req.body.email },
+    });
+
+    if (typeof user !== "undefined" && user.length > 0) {
+      res.status(401).json({ message: "User already exists, Please Login", success: false });
+    } else {
+
+      const saltRounds = 10;
+      const hash = await bcrypt.hash(password, saltRounds);
+      if (hash) {
+        const insertData = await User.create({ name, email, password: hash, phone: phone });
+        res.status(201).json({ message: "Sign Up Succesful", success: true });
       }
-    } catch (error) {
-      console.log(error);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 
-  exports.postLogin = async(req,res,next)=>{
+exports.generateAccessToken = (id, name) => {
+  return jwt.sign({ userId: id, name: name }, process.env.JSONTOKEN_SECRET);
+};
 
-  };
+
+exports.postLogin = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const userDeatails = await User.findAll({
+      attributes: ['email', 'password', 'id'],
+      where: { email: email }
+    });
+
+    if (typeof userDeatails !== "undefined" && userDeatails.length > 0) {
+      const match = await bcrypt.compare(password, userDeatails[0].password);
+      if (match) {
+        res.status(200).json({ message: "User Login Succesful", token: this.generateAccessToken(userDeatails[0].id, userDeatails[0].name), success: true });
+      } else {
+        res.status(401).json({ message: "User Not authorized", success: false });
+      }
+    } else {
+      res.status(404).json({ message: "User Does not exist", success: false });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
