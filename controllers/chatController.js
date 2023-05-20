f3bca245585259a3c71a583171729800e9e5b23a
syeef <syeefislam@yahoo.com>
2023-05-20 19:10:14 +0530

    before uploading to aws again

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,14 +4,12 @@ const Group = require("../models/groupModel");
 const { Op } = require("sequelize");
 const S3Service = require("../services/s3services");
 
-
-
-
 const jwt = require("jsonwebtoken");
 
 module.exports.respond = function (socket_io) {
   //console.log("called_socket io controller");
 
+  //ADD MESSAGE TO DB
   socket_io.on("send_message", async (obj) => {
     //console.log(obj);
     //if no group id null
@@ -22,42 +20,40 @@ module.exports.respond = function (socket_io) {
       const group_id = obj.group_id;
       const content = obj.content;
 
-      if(obj.fileInput){
+      if (obj.fileInput) {
         const fileBuffer = obj.fileInput[0];
         const fileName = obj.file_name;
         const fileSize = obj.file_size;
         const fileType = obj.file_type;
-        const newFilename = (new Date().getTime()+fileName).toString();
-        var fileURL = await S3Service.uploadToS3(fileBuffer,newFilename);
+        const newFilename = (new Date().getTime() + fileName).toString();
+        var fileURL = await S3Service.uploadToS3(fileBuffer, newFilename);
       }
-      if(!fileURL){
-        var fileURL='';
+      if (!fileURL) {
+        var fileURL = "";
       }
       let insertData = await Chat.create({
         message: content,
         groupId: parseInt(group_id),
         userId: userObj.userId,
-        fileurl:fileURL
+        fileurl: fileURL,
       });
-      
+
       //console.log(insertData);
       const chatId = insertData.id;
       const message = insertData.message;
       const groupid = insertData.groupId;
       const userId = insertData.userId;
 
-      const chatData = await Chat.findOne(
-        {
-          where: { groupId: groupid,id:chatId},
-          order: [["createdAt", "DESC"]],
-          include: [
-            {
-              model: User,
-              attributes: ["name"],
-            },
-          ],
-        },
-      );
+      const chatData = await Chat.findOne({
+        where: { groupId: groupid, id: chatId },
+        order: [["createdAt", "DESC"]],
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+      });
       //console.log("singale chat", JSON.stringify(chatData, null, 2));
 
       const group = await Group.findOne({ where: { id: group_id } });
@@ -66,62 +62,69 @@ module.exports.respond = function (socket_io) {
       socket_io.broadcast
         .to(groupName)
         .emit("recive_message", { chatData: chatData });
+
+      socket_io.emit("recive_message", { chatData: chatData });
     }
   });
 
-  // function parseJwt(token) {
-  //   return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-  // }
-
-  // function findUserById(id){
-  //   try{
-  //     const user = User.findOne({where:{id:id}});
-  //     return user;
-  //   }catch(error){
-  //     console.log(error);
-  //   }
-  // }
-
-  // socket.on("get_message", (obj) => {
-  //   console.log(obj);
-  // });
-
+  //JOIN GROUP
   socket_io.on("join_group", async (obj) => {
     //console.log(obj);
     const group_id = obj.group_id;
 
-    const group = await Group.findOne({ where: { id: group_id } });
-    const groupName = group.name;
-    //console.log("group details",groupName);
-    const chatData = await Chat.findAll({
-      where: { groupId: group_id },
-      order: [["createdAt", "ASC"]],
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-    console.log("chat data of group", JSON.stringify(chatData, null, 2));
-    socket_io.join(groupName);
-    socket_io.emit("recive_message", {
-      groupName: groupName,
-      chatData: chatData,
-    });
+    const userObj = jwt.verify(obj.token, process.env.JSONTOKEN_SECRET);
+    if (userObj) {
+      const group = await Group.findOne({ where: { id: group_id } });
+      const groupName = group.name;
+      //console.log("group details",groupName);
+      const chatData = await Chat.findAll({
+        where: { groupId: group_id },
+        order: [["createdAt", "ASC"]],
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+      });
+      //console.log("chat data of group", JSON.stringify(chatData, null, 2));
+      socket_io.join(groupName);
+      socket_io.emit("recive_message", {
+        groupName: groupName,
+        chatData: chatData,
+      });
+
+      socket_io.emit("status_message", {
+        message: `${userObj.name} connected to ${groupName}`,
+      });
+
+      //to all user of group
+      socket_io.broadcast
+        .to(groupName)
+        .emit("status_message", { message: `${userObj.name} joined` });
+    }
   });
 
+  //LEAVE GROUP
   socket_io.on("disconnect_group", async (obj) => {
     console.log(obj);
-    const group_id = obj.group_id;
-    const group = await Group.findOne({ where: { id: group_id } });
-    const groupName = group.name;
-    socket_io.leave(groupName);
-    
-    socket_io.emit("status_message", {
-      message: `Group disconnected ${groupName}`,
-    });
-    //console.log('A user disconnected');
+    const userObj = jwt.verify(obj.token, process.env.JSONTOKEN_SECRET);
+    console.log(userObj);
+    if (userObj) {
+      const group_id = obj.group_id;
+      const group = await Group.findOne({ where: { id: group_id } });
+      const groupName = group.name;
+      socket_io.leave(groupName);
+
+      socket_io.emit("status_message", {
+        message: `${userObj.name} disconnected from ${groupName}`,
+      });
+
+      //to all user of group
+      socket_io.broadcast
+        .to(groupName)
+        .emit("status_message", { message: `${userObj.name} left` });
+    }
   });
 
   // socket_io.on('error', (err) => {
@@ -129,12 +132,24 @@ module.exports.respond = function (socket_io) {
   // });
 };
 
+// function parseJwt(token) {
+//   return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+// }
 
+// function findUserById(id){
+//   try{
+//     const user = User.findOne({where:{id:id}});
+//     return user;
+//   }catch(error){
+//     console.log(error);
+//   }
+// }
 
-
+// socket.on("get_message", (obj) => {
+//   console.log(obj);
+// });
 
 //old http code
-
 
 // exports.addChat = async (req, res, next) => {
 //   // console.log(req.user.id);
